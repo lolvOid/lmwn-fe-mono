62bb3b3b446e958664a01c5667f353f35ca3da0f
lolvOid <linhtet1998itachi@gmail.com>
2024-02-26 14:40:52 +0700

    feat: initial-setup

---
+++ packages/web/.prettierrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    useTabs: false,
+    printWidth: 100,
+    tabWidth: 2,
+    singleQuote: true,
+    semi: true,
+    trailingComma: 'all',
+    arrowParens: "always",
+    overrides: [
+        {
+            files: '*.{js,jsx,tsx,ts,scss,json,html}',
+            options: {
+                tabWidth: 4,
+            },
+        },
+    ],
+    endOfLine: "lf",
+  };
